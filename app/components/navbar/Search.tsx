87e2ccbd044927caf07c13f2e0472ccdbf393df3
Tony Dugué <duguetony@gmail.com>
2023-05-13 10:58:39 +0200

    add navbar UI

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { BiSearch } from "react-icons/bi";
+
+const Search = () => {
+  return (
+    <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
+      <div className="flex flex-row items-center justify-between">
+        <div className="text-sm font-semibold px-6">N&apos;importe où</div>
+
+        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
+          Une semaine
+        </div>
+
+        <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
+          <div className="hidden sm:block pr-1">Ajouter des voyageurs</div>
+          <div className="p-2 bg-rose-500 rounded-full text-white">
+            <BiSearch size={18} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
