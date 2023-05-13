900fbe0d4491226bd8132b441379de51800152ac
Tony Dugué <duguetony@gmail.com>
2023-05-13 20:10:55 +0200

    add Register Modal UI

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
+import Modal from "./Modal";
+import Input from "../inputs/Input";
+import Heading from "../Heading";
+import Button from "../Button";
+
+const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // React-use-form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: { name: "", email: "", password: "" },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        toast.error("une erreur s'est produite");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Bienvenue sur Airbnb" subtitle="Créer un compte !" />
+
+      <Input
+        id="email"
+        label="Adresse e-mail"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Nom"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Mot de passe"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button outline label="Continuer avec Google" icon={FcGoogle} onClick={() => {}} />
+      <Button outline label="Continuer avec Github" icon={AiFillGithub} onClick={() => {}}/>
+
+      <div className=" text-neutral-500 text-center mt-4 font-light">
+        <p>Vous avez déjà un compte ? <span onClick={() => {}} className="text-neutral-800 cursor-pointer hover:underline"> Connexion</span></p>
+      </div>
+    </div>
+  )
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Inscription"
+      actionLabel="Continuer"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
+};
+
+export default RegisterModal;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,13 +4,15 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleOpen = useCallback( () => {
-    setIsOpen( value => !value)
-  }, [])
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
 
   return (
     <div className="relative">
@@ -39,12 +41,11 @@ const UserMenu = () => {
           <div className="flex flex-col cursor-pointer">
             <>
               <MenuItem onClick={() => {}} label="Connexion" />
-              <MenuItem onClick={() => {}} label="Inscription" />
+              <MenuItem onClick={registerModal.onOpen} label="Inscription" />
             </>
           </div>
         </div>
       )}
-
     </div>
   );
 };
